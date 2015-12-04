df5f8cc55c2bbf66910b8834807113275b7cf847
Sebastian Abondano <sabondano1@gmail.com>
2015-12-04 16:48:06 -0700

    Get register route to work
    Why:
    
    * for a user to be able to register with their name
    
    This change addresses the need by:
    
    * building the register route
    * adding necessary code to register/template
    * adding addNew action to register route
    * adding a data service (mock service)
    * adding a user model
    * adding a RESTAdapter
    * adding a RESTSerializer
    * installing nedb (an embedded data store for development)
    * installing body-parser (dev only)
    * customizing mock service generated to use body-parser
    * customizing mock service generated to create an embedded table using
      nedb
    * customizing post url to create a new record

--- app/register/route.js
+++ app/register/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addNew: function() {
+      let user = this.store.createRecord('user', {
+        name: this.controller.get('name')
+      });
+      user.save().then(() => {
+        console.log('save succuessful');
+        this.controller.set('message', 'A new user with the name "'
+                            + this.controller.get('name')
+                            + '"was added!');
+        this.controller.set('name', null);
+      }, function() {
+       console.log('save failed');
+      });
+    }
+  } 
 });
