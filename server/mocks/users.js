72055f09de68b34fc856b2c93012fe169ce2f165
Sebastian Abondano <sabondano1@gmail.com>
2015-12-06 17:13:56 -0700

    Add login functionality
    Why:
    
    * A user should be able to login with their name/username
    
    This change addresses the need by:
    
    * adding a login action to login route
    * adding a login template
    * adding a link to login template within the application template
    * modify mock server's GET / to look for user in DB and include it in
      respond

--- server/mocks/users.js
+++ server/mocks/users.js
@@ -34,8 +34,10 @@ module.exports = function(app) {
   // For now, we won't touch the rest of this code, which was created by
   // Ember CLI
   usersRouter.get('/', function(req, res) {
-    res.send({
-      'users': []
+    userDB.find(req.query).exec(function(error, users) {
+      res.send({
+        'users': users
+      });
     });
   });
 
