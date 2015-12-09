125ab96929e4da0a8e120cce41dc6aa71a04d35a
Sebastian Abondano <sabondano1@gmail.com>
2015-12-08 22:35:24 -0600

    Add model hook to notebooks route

--- app/notebooks/route.js
+++ app/notebooks/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.query('notebook', { user: params.user_id });
+  },
+  actions: {
+  }
 });
