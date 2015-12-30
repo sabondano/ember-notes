7c9b0a2bcbf69c5e631b464081be1db27421881a
Sebastian Abondano <sabondano1@gmail.com>
2015-12-30 14:19:48 -0500

    Add isValidLength Utility and use in addTitle (for notes)

--- app/notebooks/notes/route.js
+++ app/notebooks/notes/route.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import isValidLength from 'ember-note/utils/is-valid-length';
 
 export default Ember.Route.extend({
   model: function(params) {
@@ -6,23 +7,30 @@ export default Ember.Route.extend({
   },
   actions: {
     addNote: function() {
-      this.store.findRecord('notebook', 
-                            this.paramsFor('notebooks.notes').notebook_id).then(
-                            (notebook) =>  {
-                              console.log(notebook);
-                              var note = this.store.createRecord('note', {
-                                title: this.controller.get('title'),
-                                notebook: notebook
+      var title = this.controller.get('title');
+      if (!isValidLength(title, 0, 140)) {
+        alert('Title must be longet than 0 '
+              + 'characters and not more than 140.');
+      }
+      else {
+        this.store.findRecord('notebook', 
+                              this.paramsFor('notebooks.notes').notebook_id).then(
+                              (notebook) =>  {
+                                console.log(notebook);
+                                var note = this.store.createRecord('note', {
+                                  title: this.controller.get('title'),
+                                  notebook: notebook
+                                });
+                                console.log(note);
+                                note.save().then(() => {
+                                  console.log('save successful'); 
+                                  this.controller.set('title', null);
+                                  this.refresh();
+                                }, function() {
+                                  console.log('save failed'); 
+                                });
                               });
-                              console.log(note);
-                              note.save().then(() => {
-                                console.log('save successful'); 
-                                this.controller.set('title', null);
-                                this.refresh();
-                              }, function() {
-                                console.log('save failed'); 
-                              });
-                            });
+      }
     },
     deleteNote: function(note) {
       console.log('deleting note with title ' + note.get('title'));
