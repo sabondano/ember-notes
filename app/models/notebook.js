93945bfefc22942a1abb9586cdb90a8b41fde391
Sebastian Abondano <sabondano1@gmail.com>
2016-01-04 21:05:30 -0500

    Add noteCount to notebook

--- app/models/notebook.js
+++ app/models/notebook.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   user: DS.belongsTo('user'),
-  notes: DS.hasMany('note')
+  notes: DS.hasMany('note'),
+  noteCount: function() {
+    return this.get('notes.length');
+  }
 });
